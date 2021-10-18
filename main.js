61ede976559c296670bd8dbbecb5f8ddaa943375
AKSHAT JAIN <akshat9983@gmail.com>
2021-10-18 18:09:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,19 +9,7 @@ const generate = () => {
     <img width="100%" height = "250px" src="${$id('banner-image').value}" alt="cover" />
 </div>` : "";
     const about_image = (input_check('about-image')) ? `<img width="55%" align="right" alt="Github" src="${$id('about-image').value}" />` : "";
-
-    const currently_work = (input_check('currently-work-prefix') && input_check('currently-work')) ? `<li>${$id('currently-work-prefix').value} <strong>${$id('currently-work').value}</strong></li>` : "";
-    const looking_to = (input_check('looking-to-prefix') && input_check('looking-to')) ? `<li>${$id('looking-to-prefix').value} <strong>${$id('looking-to').value}</strong></li>` : "";
-    const looking_for = (input_check('looking-for-prefix') && input_check('looking-for')) ? `<li>${$id('looking-for-prefix').value} <strong>${$id('looking-for').value}</strong></li>` : "";
-
-    const currently_learning = (input_check('currently-learning-prefix') && input_check('currently-learning')) ? `<li>${$id('currently-learning-prefix').value} <strong>${$id('currently-learning').value}</strong></li>` : "";
-    const ask_me = (input_check('ask-me-prefix') && input_check('ask-me')) ? `<li>${$id('ask-me-prefix').value} <strong>${$id('ask-me').value}</strong></li>` : "";
-    const reach_me = (input_check('reach-me-prefix') && input_check('reach-me')) ? `<li>${$id('reach-me-prefix').value} <strong>${$id('reach-me').value}</strong></li>` : "";
-    const my_project = (input_check('my-project-prefix') && input_check('my-project')) ? `<li>${$id('my-project-prefix').value} <a href="${$id('my-project').value}" >${$id('my-project').value}</a></li>` : "";
-    const blog = (input_check('blog-prefix') && input_check('blog')) ? `<li>${$id('blog-prefix').value} <a href="${$id('blog').value}" >${$id('blog').value}</a></li>` : "";
-    const my_experience = (input_check('my-experience-prefix') && input_check('my-experience')) ? `<li>${$id('my-experience-prefix').value} <a href="${$id('my-experience').value}" >${$id('my-experience').value}</a></li>` : "";
-    const fun_fact = (input_check('fun-fact-prefix') && input_check('fun-fact')) ? `<li>${$id('fun-fact-prefix').value} <strong>${$id('fun-fact').value}</strong></li>` : "";
-
+    
     const github = $id('github-username').value;
     const wakatime = $id('wakatime-username').value;
     const twitter = (input_check('twitter-username')) ? `<a href="https://twitter.com/${$id('twitter-username').value}" target="blank"><img align="center" src="${baseUrl}twitter.svg" alt="${$id('twitter-username').value}" height="30" width="40" /></a>` : "";
@@ -49,20 +37,20 @@ const generate = () => {
     const support = (input_check('support')) ? `<h2 >Support:</h2>
         <p><a href="https://www.buymeacoffee.com/${$id('support').value}"> <img align="left" src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" height="50" width="210" alt="${$id('support').value}" /></a></p><br><br>` : "";
 
-    const visitors_count = ($id('visitors-count').checked && input_check('github-username')) ? `<p align="left"><img src="https://komarev.com/ghpvc/?username=${github}&label=Profile%20views&color=0f3de6&style=flat-square" alt="${github}" /></p>` : "";
-    const github_trophy = ($id('github-trophy').checked && input_check('github-username')) ? `<p align="left"><a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=${github}" alt="${github}" /></a></p>` : "";
-    const github_metrices = ($id('github-metrices').checked && input_check('github-username')) ? `<p><img align="left" src="https://metrics.lecoq.io/${github}" alt="${github}" /></p>` : "";
-    const activity_graph = ($id('activity-graph').checked && input_check('github-username')) ? `<p><img align="center" src="https://activity-graph.herokuapp.com/graph?username=${github}" alt="${github}" width="100%" /></p>` : "";
-    const github_stats = ($id('github-stats').checked && input_check('github-username')) ? `<p><img align="center" src="https://github-readme-stats.vercel.app/api?username=${github}&show_icons=true&locale=en" alt="${github}" /></p>` : "";
-    const top_skills = ($id('top-skills').checked && input_check('github-username')) ? `<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=${github}&show_icons=true&locale=en&layout=compact" alt="${github}" /></p>` : "";
-    const github_streak = ($id('github-streak').checked && input_check('github-username')) ? `<p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=${github}&theme=dark" alt="${github}" /></p>` : "";
+    const visitors_count = ($id('visitors-count').checked && input_check('github-username')) ? `<p align="left"><img src="https://komarev.com/ghpvc/?username=${github}&label=Profile%20views&color=0f3de6&style=flat-square" alt="${$id('title').value}'s Visitor Count" /></p>` : "";
+    const github_trophy = ($id('github-trophy').checked && input_check('github-username')) ? `<p align="left"><a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=${github}" alt="${$id('title').value}'s Github Trophies" /></a></p>` : "";
+    const github_metrices = ($id('github-metrices').checked && input_check('github-username')) ? `<p><img align="left" src="https://metrics.lecoq.io/${github}" alt="${$id('title').value}'s Github Metrices" /></p>` : "";
+    const activity_graph = ($id('activity-graph').checked && input_check('github-username')) ? `<p><img align="center" src="https://activity-graph.herokuapp.com/graph?username=${github}" alt="${$id('title').value}'s Activity Graph" width="100%" /></p>` : "";
+    const github_stats = ($id('github-stats').checked && input_check('github-username')) ? `<p><img align="center" src="https://github-readme-stats.vercel.app/api?username=${github}&show_icons=true&locale=en" alt="${$id('title').value}'s Github Stats" /></p>` : "";
+    const top_skills = ($id('top-skills').checked && input_check('github-username')) ? `<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=${github}&show_icons=true&locale=en&layout=compact&langs_count=10" alt="${$id('title').value}'s Top Skills" /></p>` : "";
+    const github_streak = ($id('github-streak').checked && input_check('github-username')) ? `<p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=${github}&theme=dark" alt="${$id('title').value}'s Github Streak" /></p>` : "";
     const wakatime_stats = ($id('wakatime-stats').checked && input_check('wakatime-username')) ? `<h2> Stuff I worked on last week  <img src = "https://media1.giphy.com/media/JZ40cnfnN11KycrvMF/giphy.gif?cid=ecf05e47a0n3gi1bfqntqmob8g9aid1oyj2wr3ds3mg700bl&rid=giphy.gif" width = 70px> </h2>
-    <p><img align="center" src="https://github-readme-stats.vercel.app/api/wakatime?username=${wakatime}&layout=compact"/></p>` : "";
-    const twitter_badge = ($id('twitter-badge').checked && input_check('twitter-username')) ? `<p align="left"> <a href="https://twitter.com/${$id('twitter-username').value}" target="blank"><img src="https://img.shields.io/twitter/follow/${$id('twitter-username').value}?logo=twitter&style=for-the-badge" alt="${$id('twitter-username').value}" /></a></p>` : "";
+    <p><img align="center" src="https://github-readme-stats.vercel.app/api/wakatime?username=${wakatime}&layout=compact" alt="${$id('title').value}'s Wakatime Stats" /></p>` : "";
+    const twitter_badge = ($id('twitter-badge').checked && input_check('twitter-username')) ? `<p align="left"> <a href="https://twitter.com/${$id('twitter-username').value}" target="blank"><img src="https://img.shields.io/twitter/follow/${$id('twitter-username').value}?logo=twitter&style=for-the-badge" alt="${$id('title').value}'s Twitter Stats" /></a></p>` : "";
     const stackoverflow_stats = ($id('stackoverflow-stats').checked && input_check('stackoverflow-username')) ? `<h2 > My Stackoverflow Stats </h2>
-    <p><img align="left" src="https://stackoverflow-card.vercel.app/?userID=${$id('stackoverflow-username').value}" alt="${$id('stackoverflow-username').value}" /></p>` : "";
+    <p><img align="left" src="https://stackoverflow-card.vercel.app/?userID=${$id('stackoverflow-username').value}" alt="${$id('title').value}'s Stackoverflow Stats" /></p>` : "";
     const joke_card = ($id('joke-card').checked) ? `<h2> Some Programming Humor for you <img align ='center' src='https://media2.giphy.com/media/UQDSBzfyiBKvgFcSTw/giphy.gif?cid=ecf05e47p3cd513axbek3f56ti3jzizq8hincw20jauyyfyw&rid=giphy.gif' width = '32px'></h2>
-    <p><img src="https://readme-jokes.vercel.app/api?theme=default" alt="Jokes Card" ></p>` : "";
+    <p><img src="https://readme-jokes.vercel.app/api?theme=default" alt="${$id('title').value}'s Jokes Card" ></p>` : "";
 
     const blog_post = (($id('dev-blog').checked && input_check('dev-username')) || ($id('medium-blog').checked && input_check('medium-username')) || ($id('personal-blog').checked && input_check('rss-username'))) ? `<h3> Blogs posts </h3>
     <!-- BLOG-POST-LIST:START -->
@@ -95,23 +83,33 @@ const generate = () => {
     $id('output').innerHTML = `${banner_image}
 ${title}
 ${subtitle}${visitors_count}${github_trophy}${twitter_badge}
-    <h2> About Me <img src = "https://media0.giphy.com/media/KDDpcKigbfFpnejZs6/giphy.gif?cid=ecf05e47oy6f4zjs8g1qoiystc56cu7r9tb8a1fe76e05oty&rid=giphy.gif" width = 100px></h2>
+<h2> About Me <img src = "https://media0.giphy.com/media/KDDpcKigbfFpnejZs6/giphy.gif?cid=ecf05e47oy6f4zjs8g1qoiystc56cu7r9tb8a1fe76e05oty&rid=giphy.gif" width = 100px></h2>
     ${about_image}
-    <ul>${currently_work}${currently_learning}${looking_to}${looking_for}${my_project}${blog}${ask_me}${reach_me}${my_experience}${fun_fact}
+    <ul>
+${currently_work()}
+${currently_learning()}
+${looking_to()}
+${looking_for()}
+${my_project()}
+${blog()}
+${ask_me()}
+${reach_me()}
+${my_experience()}
+${fun_fact()}
     </ul>
-    ${blog_post}
-    <h2> Connect with me <img src='https://raw.githubusercontent.com/ShahriarShafin/ShahriarShafin/main/Assets/handshake.gif' width="100px"> </h2>
-    <p align="left">
-    ${codepen}${dev}${twitter}${linkedin}${stackoverflow}${codesandbox}${kaggle}${facebook}${instagram}${dribbble}${behance}${medium}${youtube}${codechef}${hackerrank}${codeforces}${leetcode}${hackerearth}${geeksforgeeks}${topcoder}${discord}${rss}
-    </p>
-    
-    <h2> Skills <img src = "https://media2.giphy.com/media/QssGEmpkyEOhBCb7e1/giphy.gif?cid=ecf05e47a0n3gi1bfqntqmob8g9aid1oyj2wr3ds3mg700bl&rid=giphy.gif" width = 32px> </h2>
+${blog_post}
+<h2> Connect with me <img src='https://raw.githubusercontent.com/ShahriarShafin/ShahriarShafin/main/Assets/handshake.gif' width="100px"> </h2>
+    <p align="left">${codepen}${dev}${twitter}${linkedin}${stackoverflow}${codesandbox}${kaggle}${facebook}${instagram}${dribbble}${behance}${medium}${youtube}${codechef}${hackerrank}${codeforces}${leetcode}${hackerearth}${geeksforgeeks}${topcoder}${discord}${rss}</p>
+<h2> Skills <img src = "https://media2.giphy.com/media/QssGEmpkyEOhBCb7e1/giphy.gif?cid=ecf05e47a0n3gi1bfqntqmob8g9aid1oyj2wr3ds3mg700bl&rid=giphy.gif" width = 32px></h2>
     <p align="left">${skill_dis}</p>
     ${support}
-    <h2> My GitHub Stats <img src='https://media1.giphy.com/media/du3J3cXyzhj75IOgvA/giphy.gif?cid=ecf05e47x2g034i9pzwtzzsd3xgg2w9nr94t4tflbbgo3008&rid=giphy.gif' width='32px'> </h2>
-    ${top_skills}${github_metrices}${github_stats}${github_streak}${activity_graph}${wakatime_stats}${stackoverflow_stats}${joke_card}
+<h2> My GitHub Stats <img src='https://media1.giphy.com/media/du3J3cXyzhj75IOgvA/giphy.gif?cid=ecf05e47x2g034i9pzwtzzsd3xgg2w9nr94t4tflbbgo3008&rid=giphy.gif' width='32px'> </h2>
+    ${github_metrices}${github_streak}${github_stats}${top_skills}${activity_graph}
+${wakatime_stats}
+${stackoverflow_stats}
+${joke_card}
     `;
-    let md = ($id('output').innerHTML).replaceAll('<li>','- ').replaceAll('</li>','').replaceAll('<strong>',' **').replaceAll('</strong>','** ').replaceAll('<ul>','').replaceAll('</ul>','');
+    let md = ($id('output').innerHTML).replaceAll('<li>', '- ').replaceAll('</li>', '').replaceAll('<strong>', ' **').replaceAll('</strong>', '** ').replaceAll('<ul>', '').replaceAll('</ul>', '');
     $id('code-block').value = md;
     $id('upload-code').value = blog_post_code;
 }
@@ -127,6 +125,36 @@ const input_check = (id) => {
 }
 const $check = () => {
     return (input_check('title') && input_check('subtitle') && input_check('github-username')) ? true : false;
+}
+const currently_work = () => {
+    return (input_check('currently-work-prefix') && input_check('currently-work')) ? `<li>${$id('currently-work-prefix').value} <strong>${$id('currently-work').value}</strong></li>` : "";
+}
+const looking_to = () => {
+    return (input_check('looking-to-prefix') && input_check('looking-to')) ? `<li>${$id('looking-to-prefix').value} <strong>${$id('looking-to').value}</strong></li>` : "";
+}
+const looking_for = () => {
+    return (input_check('looking-for-prefix') && input_check('looking-for')) ? `<li>${$id('looking-for-prefix').value} <strong>${$id('looking-for').value}</strong></li>` : "";
+}
+const currently_learning = () => {
+    return (input_check('currently-learning-prefix') && input_check('currently-learning')) ? `<li>${$id('currently-learning-prefix').value} <strong>${$id('currently-learning').value}</strong></li>` : "";
+}
+const ask_me = () => {
+    return (input_check('ask-me-prefix') && input_check('ask-me')) ? `<li>${$id('ask-me-prefix').value} <strong>${$id('ask-me').value}</strong></li>` : "";
+}
+const reach_me = () => {
+    return (input_check('reach-me-prefix') && input_check('reach-me')) ? `<li>${$id('reach-me-prefix').value} <strong>${$id('reach-me').value}</strong></li>` : "";
+}
+const my_project = () => {
+    return (input_check('my-project-prefix') && input_check('my-project')) ? `<li>${$id('my-project-prefix').value} <a href="${$id('my-project').value}" >${$id('my-project').value}</a></li>` : "";
+}
+const blog = () => {
+    return (input_check('blog-prefix') && input_check('blog')) ? `<li>${$id('blog-prefix').value} <a href="${$id('blog').value}" >${$id('blog').value}</a></li>` : "";
+}
+const my_experience = () => {
+    return (input_check('my-experience-prefix') && input_check('my-experience')) ? `<li>${$id('my-experience-prefix').value} <a href="${$id('my-experience').value}" >${$id('my-experience').value}</a></li>` : "";
+}
+const fun_fact = () => {
+    return (input_check('fun-fact-prefix') && input_check('fun-fact')) ? `<li>${$id('fun-fact-prefix').value} <strong>${$id('fun-fact').value}</strong></li>` : "";
 }
 // setInterval(function () { ($check()) ? $id('generate-btn').removeAttribute('disabled') : $id('generate-btn').setAttribute('disabled', 'true') }, 500);
 const add_ons = (name) => {
